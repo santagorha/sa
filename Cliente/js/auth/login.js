96689fd326c4dc1654fc4db4bf1e1a43912e0337
nicovivas28 <nicovivas28@gmail.com>
2017-09-12 17:44:01 -0500

    Modify: Authentification fail look
    Signed-off-by: nicovivas28

--- Cliente/js/auth/login.js
+++ Cliente/js/auth/login.js
@@ -1,22 +1,21 @@
 document.addEventListener('init', function(event) {
   var page = event.target;
-  var username = document.getElementById('username');
-  var password = document.getElementById('password');
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
   var error = document.getElementById('errorMessage');
   if (page.id === 'login') {
     page.querySelector('#login').onclick = function() {
       //Hace validación de usuario
       //Toca buscar una forma segura ya que hacer esta comparación acá no lo es
-      if (username.value === 'user' && password.value === 'secret') {
+      if (username === 'user' && password === 'secret') {
         document.querySelector('#myNavigator').pushPage('home.html', {
           data: {
             title: 'TODO'
           }
         });
       } else {
-        username.style.backgroundColor = '#ffcdd2';
-        password.style.backgroundColor = '#ffcdd2';
-        error.style.display = 'inline';
+        //ons.notification.alert('Datos de usuario incorrectos.');
+        toggleToast();
       }
     };
     page.querySelector('#reestablecer').onclick = function(){
@@ -26,3 +25,8 @@ document.addEventListener('init', function(event) {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
 });
+
+
+function toggleToast() {
+  document.querySelector('ons-toast').toggle();
+}
