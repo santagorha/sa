3737a844e40b1e36fcd5b0e119ce12b6d9b1bee9
nicovivas28 <nicovivas28@gmail.com>
2017-09-19 16:45:13 -0500

    Separate Auth page and App page Save a session token in localStorage of html5
    Signed-off-by: nicovivas28

--- Cliente/js/auth/login.js
+++ Cliente/js/auth/login.js
@@ -4,48 +4,15 @@ var authUser = function() {
 
   //Hace validación de usuario
   //Toca buscar una forma segura ya que hacer esta comparación acá no lo es
-  if (username === 'bob' && password === 'secret') {
-    document.querySelector('#appNavigator').pushPage('home.html');
+  if (username && password === 'secret') {
+      localStorage.setItem('myPoliUser', username);
+      window.location.replace("index.html");
   }
   else {
-    ons.notification.toast('Datos de usuario incorrectos.');
+    ons.notification.alert('Datos de usuario incorrectos.');
   }
 };
 
 var reestablecer = function() {
   window.open("https://cpo.poligran.edu.co/");
 };
-
-window.fn = {};
-
-window.fn.toggleMenu = function () {
-  document.getElementById('appSplitter').right.toggle();
-};
-
-window.fn.loadView = function (index) {
-  document.getElementById('appTabbar').setActiveTab(index);
-  document.getElementById('sidemenu').close();
-};
-
-window.fn.loadLink = function (url) {
-  window.open(url, '_blank');
-};
-
-window.fn.pushPage = function (page, anim) {
-  if (anim) {
-    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
-  } else {
-    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
-  }
-};
-
-ons.ready(function () {
-  const sidemenu = document.getElementById('appSplitter');
-  ons.platform.isAndroid() ? sidemenu.right.setAttribute('animation', 'overlay') : sidemenu.right.setAttribute('animation', 'reveal');
-
-  document.querySelector('#tabbar-page').addEventListener('postchange', function(event) {
-    if (event.target.matches('#appTabbar')) {
-      event.currentTarget.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label');
-    }
-  });
-});
