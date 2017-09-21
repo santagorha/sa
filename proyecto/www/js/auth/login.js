4e4977f4166f0c4eafcb5bd6d18393d7fb666813
nicovivas28 <nicovivas28@gmail.com>
2017-09-21 14:14:41 -0500

    Change: Style of login page Change button style
    Signed-off-by: nicovivas28

---
+++ proyecto/www/js/auth/login.js
@@ -0,0 +1,22 @@
+var myUser = localStorage.getItem("myPoliUser");
+
+document.addEventListener('init', function(event) {
+  if (myUser) {
+    window.location.replace("content.html");
+  }
+});
+
+var authUser = function() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  //Hace validación de usuario
+  //Toca buscar una forma segura ya que hacer esta comparación acá no lo es
+  if (username && password === 'secret') {
+    localStorage.setItem('myPoliUser', username);
+    window.location.replace("content.html");
+  }
+  else {
+    ons.notification.alert('Datos de usuario incorrectos.');
+  }
+};
