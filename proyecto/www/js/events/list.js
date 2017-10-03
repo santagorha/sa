d25136b0ef6fd8a6492bb80dd43771ee86eb69f4
nicovivas28 <nicovivas28@gmail.com>
2017-10-02 21:38:56 -0500

    Generate dynamic filters with db

--- proyecto/www/js/events/list.js
+++ proyecto/www/js/events/list.js
@@ -1,20 +1,20 @@
 document.addEventListener('deviceready', function(event) {
   createEventos("list-eventos", eventos.lista);
+  createFiltros("filter-lugar", itemPerFilter.lugar);
+  createFiltros("filter-categoria", itemPerFilter.categoria);
+  createFiltros("filter-facultad", itemPerFilter.facultad);
 
   $(".filterHome").toggle();
 });
-
 
 var toggleFilter = function(filtro) {
   $(filtro).toggle("slow");
 };
 
-
 var createEventos = function(node, params) {
   var itemNode = document.getElementById(node);
   var htmlElements = "";
   for(i in params) {
-
     htmlElement += "<ons-list-item>\n";
     htmlElement += params[i].nombre + params[i].categoria + params[i].lugar + params[i].diahora + "\n";
     htmlElement += "</ons-list-item>\n";
@@ -23,16 +23,16 @@ var createEventos = function(node, params) {
   itemNode.innerHTML = htmlElement;
 };
 
+var createFiltros = function(node, params) {
+  var itemNode = document.getElementById(node);
+  var htmlElements = "";
+  for(i in params) {
+    htmlElement += "<ons-list-item>\n";
+    htmlElement += "<ons-checkbox value=\"" + params[i].key + "\">\n";
+    htmlElement += params[i].nombre + "\n";
+    htmlElement += "</ons-checkbox>\n";
+    htmlElement += "</ons-list-item>\n";
+  }
 
-
-// var createFiltros = function(node, params) {
-//     var itemNode = document.getElementById(node);
-//     var htmlElements = "";
-//     for(i in params) {
-//       var htmlElement  = "";
-//       htmlElement += "<ons-list-item>\n";
-//       htmlElement += "<ons-checkbox class=\"filter-" + params[0] + "\" value=\"something\" checked>GENERADO</ons-checkbox>\n";
-//       htmlElement += "</ons-list-item>\n";
-//     }
-//     node.innerText = htmlElement;
-// };
+  itemNode.innerHTML = htmlElement;
+};
