37e5606495dbf93f999f6118a463b6800843cf38
David Eduardo Gualteros <degualteros@poligran.edu.co>
2017-10-27 19:36:37 -0500

    Se añade clase de servicio
    Se añade clase que realiza consulta necesaria para el modelo generando
    la totalidad de creditos del usuario

--- server/api/controllers/ConstrollerHistoryEvent.php
+++ server/api/controllers/ConstrollerHistoryEvent.php
@@ -6,12 +6,11 @@ class ControllerHistoryEvent {
   public function __construct($model) {
     $this->model = $model;
   }
-
+ 
   public function getHistoryEvent($data) {
     $username = $data["user"];
-    $this->model->entity = "EVENTO NATURAL JOIN EVENTO_USUARIO NATURAL JOIN USUARIO";
-    $this->model->id = array("NOMBRE_USUARIO" => $username,
-    "MARCA_AGREGADO" => true);
+    $this->model->entity = "SUM(";
+    $this->model->id = array("NOMBRE_USUARIO" => $username, "MARCA_AGREGADO" => true);
     $eventList = $this->model->get();
     $response = array(
       "codeStatus" => OK,
@@ -22,27 +21,15 @@ class ControllerHistoryEvent {
 
   /* por si la consulta no funciona y la quiero hacer diferente
   public function setAsistencia($data) {
-
     $eventIds = $data["eventos"];
-
-
-
     $query = "UPDATE EVENTO_USUARIO SET ASISTIDO = TRUE WHERE ID_EVENTO_USUARIO IN ({$eventIds})";
-
     $model = new Model();
-
     $result = $model->setQuery($query);
-
     $response = array(
-
       "codeStatus" => OK,
-
       "message" => $result      
-
     );
-
     return $response;
-
   }
 */
 }
