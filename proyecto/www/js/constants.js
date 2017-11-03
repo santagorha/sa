49004c27b870a7b8d9bb64cc636ef7bcc05e205c
willihamer <whpatarroyo@poligran.edu.co>
2017-11-02 20:07:20 -0500

    Ingreso constant js
    Ingreso constant JS

--- proyecto/www/js/constants.js
+++ proyecto/www/js/constants.js
@@ -4,3 +4,4 @@ const URL_USER_SERVICE = "https://apppracticaaplicada.herokuapp.com/server/user"
 const URL_USERS_EVENT_SERVICE = "https://polipa2017.000webhostapp.com/server/usuariosEvento";
 const URL_CONFIRM_EVENT_SERVICE = "https://polipa2017.000webhostapp.com/server/asistencia";
 const URL_UNKNOW_USER_SERVICE = "https://polipa2017.000webhostapp.com/server/usuarioNuevoEvento";
+const URL_COMMENT_SERVICE = "https://polipa2017.000webhostapp.com/server/consultarComentario";
