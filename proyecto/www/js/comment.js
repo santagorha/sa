d1cb8666b7e68c9a447a31c6124ce423d2a123db
willihamer <whpatarroyo@poligran.edu.co>
2017-11-06 10:55:17 -0500

    Modificacion visualizacion comentarios
    Modificacion visualizacion comentarios

--- proyecto/www/js/comment.js
+++ proyecto/www/js/comment.js
@@ -1,18 +1,45 @@
+var idevento = 1;
+
 function comment(){
+  console.log("hola");
   $.ajax({
-    data: {"evento": 1},
-    dataType: "json",
+    data: {"evento": idevento},
     error: function (err) {
-      var t = 1;
-      document.write(err.responseText);
+      console.log(err.responseText);
       console.log(err.responseJSON);
     },
     success: function (data) {
       if (data.codeStatus === 200) {
-        document.write(JSON.stringify(data));
+        //alert(JSON.stringify(data.comentarios));
+        llenarComentario(data.comentarios);
       }
     },
-    type: "post",
+    type: "get",
     url: URL_COMMENT_SERVICE
   });
 }
+
+
+
+function llenarComentario(comentarios){
+  var comElement = document.getElementById('commentElement');
+  var htmlEl = "";
+    
+  for(let comentario of comentarios){
+
+    htmlEl += `<label> ID_COMENTARIO ${comentario.ID_COMENTARIO} </label></br>`;
+    htmlEl += `<label> ID_USUARIO ${comentario.ID_USUARIO} </label></br>`;
+    htmlEl += `<label> ID_EVENTO ${comentario.ID_EVENTO} </label></br>`;
+    htmlEl += `<label> FECHA ${comentario.FECHA} </label></br>`;
+    htmlEl += `<label> TEXTO ${comentario.TEXTO} </label></br>`;
+
+  }
+  comElement.innerHTML = htmlEl;
+
+}
+
+
+
+document.addEventListener('init', comment());
+
+//$(document.ready(comment()));
