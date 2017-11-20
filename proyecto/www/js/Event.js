9053a86d30dac28c6542ed36c84b2f8bd3a59831
Nicolas Vivas <nicovivas28@gmail.com>
2017-11-19 21:13:21 -0500

    That bar doesn't work

--- proyecto/www/js/Event.js
+++ proyecto/www/js/Event.js
@@ -14,7 +14,7 @@ function getEventos ( ) { // Consumo de servicio
       ons.notification.alert('Problemas con  la conexión');
     }
   }).done(function(data, textStatus, jqXHR) {
-       createEventos(data);      
+       createEventos(data);
   });
 };
 
@@ -25,18 +25,19 @@ var createEventos = function(params) {
   var htmlElement = "";
   var i = 0;
   for(i in params) {
-    htmlElement += "<ons-row'>\n";
-    htmlElement += "<ons-card  class='cojaEvento'>\n";
+    htmlElement += "<ons-list-item'>\n";
+    htmlElement += "<ons-card>\n";
     htmlElement += "<span class='id'>"+params[i].ID_EVENTO +"</span>\n";
     htmlElement += "<div class='nombre'>"+params[i].NOMBRE_EVENTO + "</div>\n";
     htmlElement += "<span class='text'>"+params[i].FECHA + "</span>\n";
     htmlElement += "<span class='text'>"+params[i].CATEGORIA + "</span>\n";
     htmlElement += "<div class='parrafe'>"+params[i].DESCRIPCION + "</div>\n";
-    htmlElement += "<ons-button class='center' >"+'Reservar'+"</ons-button>\n";
+    htmlElement += "<ons-button class='center'>"+'Reservar'+"</ons-button>\n";
     htmlElement += "<ons-button >"+'Favorito'+"</ons-button>\n";
     htmlElement += "<ons-button >"+'Ver más'+"</ons-button>\n";
     htmlElement += "</ons-card >\n";
-    htmlElement += "</ons-row>\n";
+    htmlElement += "</ons-list-item>\n";
     }
   itemNode.innerHTML = htmlElement;
+  $(window).trigger('resize');
 };
